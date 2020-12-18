3a7b2a96f2068aca9414f9f721b5504b2ddc44e8
bupt-wtyj <72364828+bupt-wtyj@users.noreply.github.com>
2020-12-18 12:10:22 +0800

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,6 @@
 
 var TXAPI_BASE_URL = "https://api.tianapi.com";  //天行数据接口域名
-var TXAPI_KEY = "";  //请填写你在天行数据www.tianapi.com获得apikey
+var TXAPI_KEY = "2ccf63ce70fd32c308e214c7525c979b";  //请填写你在天行数据www.tianapi.com获得apikey
 
 const formatTime = date => {
   const year = date.getFullYear()
